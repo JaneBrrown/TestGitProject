3a9f1af18e78cac6c872259a4ba12f7cfbd2edf8
Jane <janebrrown@gmail.com>
2024-09-30 18:24:09 +0400

    lesson 3: create, update, delete user

--- core/contracts.py
+++ core/contracts.py
@@ -1,13 +1,13 @@
 USER_DATA_SCHEME = {
-    "type" : "object",
-    "properties" : {
-        "id" : {"type" : "number"},
-        "email" : {"type" : "string"},
-        "first_name" : {"type" : "string"},
-        "last_name" : {"type" : "string"},
-        "avatat" : {"type" : "string"}
+    "type": "object",
+    "properties": {
+        "id": {"type": "number"},
+        "email": {"type": "string"},
+        "first_name": {"type": "string"},
+        "last_name": {"type": "string"},
+        "avatat": {"type": "string"}
     },
-    "required" : ["id", "email", "first_name", "last_name", "avatar"]
+    "required": ["id", "email", "first_name", "last_name", "avatar"]
 
 }
 
@@ -22,4 +22,24 @@ COLOUR_DATA_SCHEME = {
 
     },
     "required": ["id", "name", "year", "color", "pantone_value"]
+}
+
+
+CREATED_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "id": {"type": "string"}
+    },
+    "required": ["id"]
+}
+
+UPDATED_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+    },
+    "required": []
 }
